1980b06f518fccbb640b2f80d45718367d0442fc
Abhishek Maddheshiya <abhishekmaddheshiya954@gmail.com>
2025-09-25 10:47:54 +0530

    commit

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -20,9 +20,9 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "../components/ui/card";
-import { Input } from "../components/ui/input";
-import { Label } from "../components/ui/label";
+} from "../Components/ui/card";
+import { Input } from "../Components/ui/input";
+import { Label } from "../Components/ui/label";
 
 function Contacts() {
   const mail = "abhishekmaddheshiya954@gmail.com";
